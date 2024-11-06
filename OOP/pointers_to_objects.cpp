86f3aae69f2f4458ee080ace5c6f853cecd590c9
Shahid Gillani <shahid5ssg@gmail.com>
2024-11-06 11:20:42 +0500

    modified dynamic memory allocation

--- OOP/pointers_to_objects.cpp
+++ OOP/pointers_to_objects.cpp
@@ -3,43 +3,53 @@
 
 using namespace std;
 
-class Student{
-	private: 
-		int rollNo;
-		string name;
-	public:
-		Student();
-		Student(string aName);
-		void setRollNo(int aRollNo);
-		int getRollNo();
+class Student {
+private:
+  int rollNo;
+  string name;
+
+public:
+  Student();
+  Student(string aName);
+  void setRollNo(int aRollNo);
+  int getRollNo();
+  string getName();
 };
 
-Student::Student(){
-	name = "Placeholder name";
-}
-Student::Student(string aName){
-	name = aName;
+Student::Student() { name = "Placeholder name"; }
+Student::Student(string aName) { name = aName; }
+
+void Student::setRollNo(int aRollNo) {
+  if (aRollNo > 0) {
+    rollNo = aRollNo;
+  } else {
+    cout << "Sahi value do bhai";
+  }
 }
 
-void Student::setRollNo(int aRollNo){
-	if(aRollNo>0){
-		rollNo = aRollNo;
-	}else{
-		cout<<"Sahi value do bhai";
-	}
-}
+int Student::getRollNo() { return rollNo; }
+string Student::getName() { return name; }
 
-int Student::getRollNo(){
-	return rollNo;
-}
+int main() {
+  //	Student obj;
+  //	Student *ptr;
+  //	ptr = &obj;
+  //	ptr->setRollNo(987);
+  //	cout<<"Roll no of ptr is: "<<ptr->getRollNo()<<endl;
+  //	cout<<"Roll no of obj is: "<<obj.getRollNo()<<endl;
 
-int main(){
-	Student obj;
-	Student *ptr;
-	ptr = &obj;
-	ptr->setRollNo(987);
-	cout<<"Roll no of ptr is: "<<ptr->getRollNo()<<endl;
-	cout<<"Roll no of obj is: "<<obj.getRollNo()<<endl;
-	system("PAUSE");
-	return 0;
+  // Student *ptr;
+  // ptr = new Student("Junaid");
+  // cout << "Default value for name is: " << ptr->getName() << endl;
+
+  int startingRollNo = 23600;
+  Student *ptr = new Student[100];
+  for (int i = 0; i < 100; i++) {
+    ptr[i].setRollNo(startingRollNo);
+    startingRollNo = startingRollNo + 1;
+  }
+
+  cout << "Roll no at 1st student is: " << ptr[0].getRollNo() << endl;
+  // system("PAUSE");
+  return 0;
 }
